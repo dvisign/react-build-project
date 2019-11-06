49d71dd8811ce0e6e91c0a91bd324bb6db7b9e47
gline <jmch0922@gmail.com>
2019-11-06 17:45:11 +0900

    풋터 레이아웃 완료

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -37,7 +37,34 @@ class Footer extends Component {
             </ul>
           </div>
           <div id="footerBody">
-
+            <div id="footerAddress">
+              <p className="avenir_light">Incheon Yeonsu-gu Songdogwahak-ro 32 Songdo Techno Park IT Center</p>
+            </div>
+            <div id="footerInfos">
+              <div className="infoLists clear">
+                <div>
+                  <span className="infoCates avenir_heavy">CEO.</span>
+                  <span className="infoConts avenir_light">Lee Seung Jin</span>
+                </div>
+                <div>
+                  <span className="infoCates avenir_heavy">Emaili.</span>
+                  <span className="infoConts avenir_light">aline@thedbrand.com</span>
+                </div>
+              </div>
+              <div className="infoLists clear">
+                <div>
+                  <span className="infoCates avenir_heavy">Tel.</span>
+                  <span className="infoConts avenir_light">02.1544.0218</span>
+                </div>
+                <div>
+                  <span className="infoCates avenir_heavy">Registration No.</span>
+                  <span className="infoConts avenir_light">133-86-21907</span>
+                </div>
+              </div>
+            </div>
+            <div id="footerCopys"> 
+              <p className="avenir_heavy">© 2019 DBRAND All rights reserved.</p>
+            </div>
           </div>
         </div>
       </div>
